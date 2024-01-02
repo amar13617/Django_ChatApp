18e29dceb14a45dbe6f4f0bfcfee26c7d54d366c
LENOVO <amar13617@gmail.com>
2024-01-02 14:06:53 +0530

    first commit

---
+++ wechatapp/chatapp/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from .models import Room,Message
+
+def rooms(request):
+    rooms=Room.objects.all()
+    return render(request, "rooms.html",{"rooms":rooms})
+
+def room(request,slug):
+    room_name=Room.objects.get(slug=slug).name
+    messages=Message.objects.filter(room=Room.objects.get(slug=slug))
+    
+    return render(request, "room.html",{"room_name":room_name,"slug":slug,'messages':messages})
+
+def index(request):
+    return render(request, 'index.html')
